9a77c8c803a2a4a578299c4a844ed0d85611af41
Chris Cresswell <christorper@gmail.com>
2022-01-12 18:58:01 -0800

    Add file and README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webpack-eslint-prettier
 quick setup for projects using webpack, eslint and prettier 
+
+save file to your desired directory and
+run $chmod 755 webpack-eslint-prettier.sh before use.
+(script will delete itself when done)
+
+Initializes a barebones webpack directory with ESlint and Prettier JS.
+
+webpack.config.js equipped to handle stylesheets, images and fonts.
+
+If you intend to use ESlint or Prettier in code editor, install respective plugins.
+
+
+
---
+++ webpack-eslint-prettier.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+npm init -y
+npm install webpack webpack-cli --save-dev
+npm install --save-dev style-loader css-loader
+touch webpack.config.js
+echo " const path = require('path');
+
+ module.exports = {
+   entry: './src/index.js',
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   module: {
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource',
+       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+     ],
+   },
+ };" >> webpack.config.js
+
+mkdir dist
+mkdir src
+touch dist/index.html
+touch src/index.js
+
+echo " <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8" />
+    <title>Project</title>
+   </head>
+   <body>
+    <script src="bundle.js"></script>
+   </body>
+ </html>" >> dist/index.html
+
+npm install eslint --save-dev
+./node_modules/.bin/eslint --init
+npm install --save-dev --save-exact prettier
+npx prettier --write .
+npm install --save-dev eslint-config-prettier
+sed -i 's/"airbnb-base"/&, "prettier"/' .eslintrc.json "airbnb-base", "prettier"]
+rm -- "$0"
